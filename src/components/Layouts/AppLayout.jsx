d35ef7ed5a7f3529e14fbbfc46db8a97d98f14d4
BegencGlyjow <bekglycev@gmail.com>
2022-04-05 13:56:28 +0500

    update

--- src/components/Layouts/AppLayout.jsx
+++ src/components/Layouts/AppLayout.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 import Player from '../Player/Player'
 import SearchBar from '../SearchBar/SearchBar'
 import { IoCompassSharp } from 'react-icons/io5'
-import { Audio } from 'react-loader-spinner'
+import { Audio, BallTriangle, Bars } from 'react-loader-spinner'
 const AppLayout = ({ children }) => {
 
   const [toggle, setToggle] = useState(false)
@@ -15,8 +15,8 @@ const AppLayout = ({ children }) => {
       {/* <Sidebar toggle={toggle} setToggle={setToggle} /> */}
       <section className={`fixed  top-0 ${toggle ? 'left-0' : '-left-28'} xl:left-0  duration-500   flex flex-col p-4 bg-gradient-to-t  from-black to-lime-900/20 items-center w-[90px] h-screen space-y-8 border-r border-lime-500/50`}>
 
-        <div className='mt-10 px-8'>
-          <Audio color="#1EB135" height="60" width="20" className="shadow-xl shadow-lime-600" />
+        <div className='mt-2 px-8'>
+          <Bars color="#1EB135" height={50} width={50} className="shadow-xl shadow-lime-600" />
         </div>
         <div className='flex flex-col space-y-8'>
           <NavLink to={'/music-app'}>
